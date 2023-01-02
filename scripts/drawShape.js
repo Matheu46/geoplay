5f9d1417281994ab35fff6fdb9ad334dd1af0ea5
Matheu46 <mathias.matheus@ymail.com>
2023-01-02 14:19:38 -0300

    desenho

--- scripts/drawShape.js
+++ scripts/drawShape.js
@@ -1,109 +1,62 @@
-// Drawing with text. Ported from Generative Design book - http://www.generative-gestaltung.de - Original licence: http://www.apache.org/licenses/LICENSE-2.0
+document.addEventListener('DOMContentLoaded', () => {
+  const pincel = {
+    ativo: false,
+    movendo: false,
+    pos: { x: 0, y: 0 },
+    posAnterior: null,
+  };
 
-// Application variables
-var position = { x: 0, y: window.innerHeight / 2 };
-var counter = 0;
-var minFontSize = 3;
-var angleDistortion = 0;
-var letters =
-  "There was a table set out under a tree in front of the house, and the March Hare and the Hatter were having tea at it: a Dormouse was sitting between them, fast asleep, and the other two were using it as a cushion, resting their elbows on it, and talking over its head. 'Very uncomfortable for the Dormouse,' thought Alice; 'only, as it's asleep, I suppose it doesn't mind.'";
+  const canvas = document.querySelector('#canvas');
+  const ctx = canvas.getContext('2d');
 
-// Drawing variables
-var canvas;
-var context;
-var mouse = { x: 0, y: 0, down: false };
-
-function init() {
-  canvas = document.getElementById('canvas');
-  context = canvas.getContext('2d');
+  // canvas.width = 700;
+  // canvas.height = 500;
   canvas.width = window.innerWidth;
   canvas.height = window.innerHeight;
 
-  canvas.addEventListener('mousemove', mouseMove, false);
-  canvas.addEventListener('mousedown', mouseDown, false);
-  canvas.addEventListener('mouseup', mouseUp, false);
-  canvas.addEventListener('mouseout', mouseUp, false);
-  canvas.addEventListener('dblclick', doubleClick, false);
-
-  window.onresize = function (event) {
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
+  const desenhar = (linha) => {
+    ctx.beginPath();
+    ctx.moveTo(linha.posAnterior.x, linha.posAnterior.y);
+    ctx.lineTo(linha.pos.x, linha.pos.y);
+    ctx.stroke();
   };
-}
 
-function mouseMove(event) {
-  mouse.x = event.pageX;
-  mouse.y = event.pageY;
-  draw();
-}
+  canvas.onmousedown = (event) => {
+    pincel.ativo = true;
+    document.getElementById('info').style.display = 'none';
+  };
+  canvas.addEventListener('touchstart', function (event) {
+    pincel.ativo = true;
+    document.getElementById('info').style.display = 'none';
+  });
 
-function draw() {
-  if (mouse.down) {
-    var d = distance(position, mouse);
-    var fontSize = minFontSize + d / 2;
-    var letter = letters[counter];
-    var stepSize = textWidth(letter, fontSize);
+  canvas.onmouseup = (event) => {
+    pincel.ativo = false;
+  };
+  canvas.addEventListener('touchend', function (event) {
+    pincel.ativo = false;
+  });
 
-    if (d > stepSize) {
-      var angle = Math.atan2(mouse.y - position.y, mouse.x - position.x);
+  canvas.onmousemove = (event) => {
+    pincel.pos.x = event.clientX;
+    pincel.pos.y = event.clientY;
+    pincel.movendo = true;
+  };
+  canvas.addEventListener('touchmove', function (event) {
+    pincel.pos.x = event.touches[0].clientX;
+    pincel.pos.y = event.touches[0].clientY;
+    pincel.movendo = true;
+  });
 
-      context.font = fontSize + 'px Georgia';
-
-      context.save();
-      context.translate(position.x, position.y);
-      context.rotate(angle);
-      context.fillText(letter, 0, 0);
-      context.restore();
-
-      counter++;
-      if (counter > letters.length - 1) {
-        counter = 0;
-      }
-
-      //console.log (position.x + Math.cos( angle ) * stepSize)
-      position.x = position.x + Math.cos(angle) * stepSize;
-      position.y = position.y + Math.sin(angle) * stepSize;
+  const ciclo = () => {
+    if (pincel.ativo && pincel.movendo && pincel.posAnterior) {
+      desenhar({ pos: pincel.pos, posAnterior: pincel.posAnterior });
+      pincel.movendo = false;
     }
-  }
-}
+    pincel.posAnterior = { x: pincel.pos.x, y: pincel.pos.y };
 
-function distance(pt, pt2) {
-  var xs = 0;
-  var ys = 0;
+    setTimeout(ciclo, 10);
+  };
 
-  xs = pt2.x - pt.x;
-  xs = xs * xs;
-
-  ys = pt2.y - pt.y;
-  ys = ys * ys;
-
-  return Math.sqrt(xs + ys);
-}
-
-function mouseDown(event) {
-  mouse.down = true;
-  position.x = event.pageX;
-  position.y = event.pageY;
-
-  document.getElementById('info').style.display = 'none';
-}
-
-function mouseUp(event) {
-  mouse.down = false;
-}
-
-function doubleClick(event) {
-  canvas.width = canvas.width;
-}
-
-function textWidth(string, size) {
-  context.font = size + 'px Georgia';
-
-  if (context.fillText) {
-    return context.measureText(string).width;
-  } else if (context.mozDrawText) {
-    return context.mozMeasureText(string);
-  }
-}
-
-init();
+  ciclo();
+});
